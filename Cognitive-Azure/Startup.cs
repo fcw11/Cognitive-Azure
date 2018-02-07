ad405e9fa2274367fcc6c4a1e85489c7aaf6e3d5
Paul Breen <git@pbreen.co.uk>
2018-02-07 22:32:15 +0000

    more security header changes

--- Cognitive-Azure/Startup.cs
+++ Cognitive-Azure/Startup.cs
@@ -33,12 +33,13 @@ namespace Cognitive_Azure
             else
             {
                 app.UseSecurityHeaders(new HeaderPolicyCollection()
-                    .AddCustomHeader("Content-Security-Policy", "default-src 'none' ; script-src 'self' https://cognitive-azure.azurewebsites.net https://maxcdn.bootstrapcdn.com https://code.jquery.com/; style-src 'self' https://maxcdn.bootstrapcdn.com; img-src 'self' ;")
+                    .AddCustomHeader("Content-Security-Policy", "default-src 'none' ; script-src 'self' https://cognitive-azure.azurewebsites.net https://maxcdn.bootstrapcdn.com https://code.jquery.com/; style-src 'self' https://maxcdn.bootstrapcdn.com; img-src 'self' ; form-action 'self' ; upgrade-insecure-requests; block-all-mixed-content; reflected-xss block; referrer no-referrer-when-downgrade;")
                     .AddCustomHeader("X-Content-Type-Options", "nosniff")
                     .AddCustomHeader("X-Frame-Options", "DENY")
                     .AddCustomHeader("X-XSS-Protection", "1; mode=block")
                     .AddCustomHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains") //"; preload")
                     .AddCustomHeader("referrer-policy", "no-referrer-when-downgrade")
+                    .RemoveCustomHeader("X-Powered-By")
                     .RemoveServerHeader()
                 );
                
