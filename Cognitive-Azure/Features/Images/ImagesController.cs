5df708c86813f07cd84b0c012f171789aaa4c78e
Paul Breen <git@pbreen.co.uk>
2018-02-15 21:22:04 +0000

    adding very basic upload functionality
    View cleanup

---
+++ Cognitive-Azure/Features/Images/ImagesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Cognitive_Azure.Features.Images
+{
+    public class ImagesController : Controller
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public ImagesController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Upload(Upload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var accountName = Configuration["BlobAccountName"];
+                var key = Configuration["BlobStorageAPIKey"];
+                var containerName = Configuration["ImageContainerName"];
+
+                var storageAccount = new CloudStorageAccount(new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(accountName, key),true);
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference(containerName);
+                var blockBlob = container.GetBlockBlobReference(Guid.NewGuid().ToString());
+                using (var stream = model.Image.OpenReadStream())
+                {
+                    blockBlob.UploadFromStreamAsync(stream).Wait();
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Cognitive-Azure/Features/Images/Upload.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Cognitive_Azure.Features.Images
+{
+    public class Upload
+    {
+        [Required]
+        [Display(Name = "Please select a file")]
+        [System.ComponentModel.DataAnnotations.DataType(DataType.Upload)]
+        public IFormFile Image { get; set; }
+
+        [FileExtensions(Extensions = (".png,.jpg,.jpeg,.gif"))]
+        public string ImageName => Image != null ? Image.FileName : string.Empty;
+    }
+}
