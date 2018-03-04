fc74021c85bdda926e76fc46af12d62aa2a6eea4
Paul Breen <git@pbreen.co.uk>
2018-03-04 23:11:32 +0000

    adding some basic validation and updating layouts

--- Cognitive-Azure/Features/Images/Upload.cs
+++ Cognitive-Azure/Features/Images/Upload.cs
@@ -1,16 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 
 namespace Cognitive_Azure.Features.Images
 {
-    public class Upload
+    public class Upload : IValidatableObject
     {
         [Required]
-        [Display(Name = "Please select a file")]
+        [Display(Name = "Please select a file", Description = "ggg")]
         [System.ComponentModel.DataAnnotations.DataType(DataType.Upload)]
         public IFormFile Image { get; set; }
 
         [FileExtensions(Extensions = (".png,.jpg,.jpeg,.gif"))]
         public string ImageName => Image != null ? Image.FileName : string.Empty;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Image.Length > 4194304)
+            {
+                yield return new ValidationResult("Image file size is too large", new[] {"Image"});
+            }        
+
+            var fileTypes = new[] {"image/png", "image/jpg", "image/png", "image/png" };
+
+            if (fileTypes.Contains(Image.ContentType))
+            {
+                yield return new ValidationResult("Incorrect image format", new[] { "Image" });
+            }
+        }
     }
 }
