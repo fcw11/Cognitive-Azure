368be4c53743ea882826f507e6e1cceb7abf71b7
Paul Breen <git@pbreen.co.uk>
2018-05-18 00:54:41 +0100

    missing js file

---
+++ Cognitive-Azure/wwwroot/js/speech.js
@@ -0,0 +1,32 @@
+﻿$(function() {
+    var recorder;
+
+    $("#startRecording").click(function() {
+        console.log("start recording");
+
+        // request permission to access audio stream
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+            recorder = new MediaRecorder(stream);
+
+            recorder.start();
+
+            recorder.ondataavailable = e => {
+                var chunks = [];
+                chunks.push(e.data);
+                if (recorder.state == 'inactive') {
+                    var blob = new Blob(chunks, { type: 'audio/webm' });
+
+                    var url = URL.createObjectURL(blob);
+
+                    $("#audioSource").attr("src", url);
+                    $("#audioControls").load();
+                };
+            }
+        }).catch(console.error);
+    });
+
+    $("#stopRecording").click(function () {
+        console.log("stop recording");
+        recorder.stop();
+    });
+});
