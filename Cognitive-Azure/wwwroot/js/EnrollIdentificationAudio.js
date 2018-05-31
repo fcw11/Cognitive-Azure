f907634c4469150981ff31b1eb8e5ca7edc5a700
Paul Breen <git@pbreen.co.uk>
2018-05-31 23:39:40 +0100

    updating other processes to use new javascript library

--- Cognitive-Azure/wwwroot/js/EnrollIdentificationAudio.js
+++ Cognitive-Azure/wwwroot/js/EnrollIdentificationAudio.js
@@ -1,93 +1,110 @@
-﻿
-var checkEnrollmentInterval = 10000;
-
-var worker = new Worker('/js/EncoderWorker.js');
-
-worker.onmessage = function (event) {
-    var form = new FormData();
-    var id = $("#Id").val();
-    form.append("__RequestVerificationToken", $("input[name='__RequestVerificationToken']").val());
-    form.append("id", id);
-    form.append("Audio", event.data.blob);
-
-    var request = new XMLHttpRequest();
-    request.open("POST", "/AudioIdentification/EnrollProfile/" + id, true);
-
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            var response = null;
-            try {
-                response = JSON.parse(request.responseText);
-            } catch (e) {
-                response = request.responseText;
-            }
-
-            console.log(response);
-        }
-    }
-    request.send(form);
-};
+﻿var checkEnrollmentInterval = 10000;
+var recorder;
+var audio_context;
 
 $(function () {
-    $("#startRecording").click(function(e) {
+    $("#startRecording").click(function (e) {
         e.preventDefault();
         startRecording();
     });
 
-    $("#stopRecording").click(function(e) {
+    $("#stopRecording").click(function (e) {
         e.preventDefault();
-        stopRecordingProcess(true);
+        stopRecording();
     });
+
+    function startRecording() {
+        navigator.getUserMedia({ audio: true }, onSuccess, onError);
+    }
+
+    function stopRecording() {
+        recorder && recorder.stop();
+        recorder.exportWAV(function (blob) {
+            createIdentificationProfile(blob);
+        });
+        recorder.clear();
+    }
+
+    function onError(e) {
+        console.error('Error', e);
+    }
+
+    function onSuccess(stream) {
+        audio_context = audio_context || new window.AudioContext;
+        var input = audio_context.createMediaStreamSource(stream);
+        recorder = new Recorder(input);
+        recorder.record();
+    }
+
+    function createIdentificationProfile(blob) {
+        var form = new FormData();
+        var id = $("#Id").val();
+        form.append("__RequestVerificationToken", $("input[name='__RequestVerificationToken']").val());
+        form.append("id", id);
+        form.append("Audio", blob);
+
+        var request = new XMLHttpRequest();
+        request.open("POST", "/AudioIdentification/EnrollProfile/" + id, true);
+
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                var response = null;
+                try {
+                    response = JSON.parse(request.responseText);
+                } catch (e) {
+                    response = request.responseText;
+                }
+            }
+        }
+        request.send(form);
+    }
+
+    function checkEnrollment() {
+        var id = $("#Id").val();
+
+        var url = "/AudioIdentification/CheckEnrollmentStatus/" + id;
+
+        $.ajax({
+            dataType: "json",
+            url: url,
+            success: updateEnrollmentStatus
+        });
+    }
+
+    function updateEnrollmentStatus(data) {
+
+        var prettifiedResponse = syntaxHighlight(data);
+
+        prettifiedResponse = prettifiedResponse.replace(/{/g, "{<br />").replace(/}/g, "<br />}").replace(/,/g, ",<br />");
+
+        $("#status").html(prettifiedResponse);
+
+        if (data.enrollmentStatus !== "Enrolled") {
+            setTimeout(checkEnrollment, checkEnrollmentInterval);
+        }
+    }
+
+    function syntaxHighlight(json) {
+        if (typeof json != 'string') {
+            json = JSON.stringify(json, undefined, 2);
+        }
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
 
     setTimeout(checkEnrollment, 1000);
 });
-
-
-function checkEnrollment() {
-    var id = $("#Id").val();
-
-    var url = "/AudioIdentification/CheckEnrollmentStatus/" + id;
-
-    $.ajax({
-        dataType: "json",
-        url: url,
-        success: updateEnrollmentStatus
-    });
-}
-
-function updateEnrollmentStatus(data) {
-
-    var prettifiedResponse = syntaxHighlight(data);
-
-    prettifiedResponse = prettifiedResponse.replace(/{/g, "{<br />").replace(/}/g, "<br />}").replace(/,/g, ",<br />");
-
-    $("#status").html(prettifiedResponse);
-
-    if (data.enrollmentStatus !== "Enrolled") {
-        setTimeout(checkEnrollment, checkEnrollmentInterval);
-    } else {
-        stopRecordingProcess(true);
-    }
-}
-
-function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-        json = JSON.stringify(json, undefined, 2);
-    }
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        } 
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
--- Cognitive-Azure/wwwroot/js/IdentifySpeaker.js
+++ Cognitive-Azure/wwwroot/js/IdentifySpeaker.js
@@ -1,35 +1,7 @@
-﻿
-var checkEnrollmentInterval = 10000;
-
-var worker = new Worker('/js/EncoderWorker.js');
-
+﻿var checkEnrollmentInterval = 10000;
 var pollingUrl;
-
-worker.onmessage = function (event) {
-
-    var form = new FormData();
-    form.append("__RequestVerificationToken", $("input[name='__RequestVerificationToken']").val());
-    form.append("Audio", event.data.blob);
-
-    var request = new XMLHttpRequest();
-    request.open("POST", "/AudioIdentification/IdentifySpeaker", true);
-
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            var response = null;
-            try {
-                response = JSON.parse(request.responseText);
-            } catch (e) {
-                response = request.responseText;
-            }
-
-            pollingUrl = response;
-            setTimeout(pollIdentificationProcess, checkEnrollmentInterval);
-            console.log(response);
-        }
-    }
-    request.send(form);
-};
+var recorder;
+var audio_context;
 
 $(function () {
     $("#startRecording").click(function (e) {
@@ -39,50 +11,98 @@ $(function () {
 
     $("#stopRecording").click(function (e) {
         e.preventDefault();
-        stopRecordingProcess(true);
+        stopRecording();
     });
-});
 
-function pollIdentificationProcess() {
-    $.ajax({
-        dataType: "json",
-        data: { url: pollingUrl },
-        url: "/AudioIdentification/PollIdentifySpeaker",
-        success: updateResult
-    });
-}
-
-function updateResult(data) {
-
-    var prettifiedResponse = syntaxHighlight(data);
-
-    prettifiedResponse = prettifiedResponse.replace(/{/g, "{<br />").replace(/}/g, "<br />}").replace(/,/g, ",<br />");
-
-    $("#status").html(prettifiedResponse);
-
-    if (data.status == "") {
-        setTimeout(pollIdentificationProcess, checkEnrollmentInterval);
+    function startRecording() {
+        navigator.getUserMedia({ audio: true }, onSuccess, onError);
     }
-}
 
-function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-        json = JSON.stringify(json, undefined, 2);
+    function stopRecording() {
+        recorder && recorder.stop();
+        recorder.exportWAV(function (blob) {
+            identifySpeaker(blob);
+        });
+        recorder.clear();
     }
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
+
+    function onError(e) {
+        console.error('Error', e);
+    }
+
+    function onSuccess(stream) {
+        audio_context = audio_context || new window.AudioContext;
+        var input = audio_context.createMediaStreamSource(stream);
+        recorder = new Recorder(input);
+        recorder.record();
+    }
+
+    function identifySpeaker(blob) {
+        var form = new FormData();
+        form.append("__RequestVerificationToken", $("input[name='__RequestVerificationToken']").val());
+        form.append("Audio", blob);
+
+        var request = new XMLHttpRequest();
+        request.open("POST", "/AudioIdentification/IdentifySpeaker", true);
+
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                var response = null;
+                try {
+                    response = JSON.parse(request.responseText);
+                } catch (e) {
+                    response = request.responseText;
+                }
+
+                pollingUrl = response;
+                setTimeout(pollIdentificationProcess, checkEnrollmentInterval);
+                console.log(response);
             }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
         }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
+        request.send(form);
+    };
+
+    function pollIdentificationProcess() {
+        $.ajax({
+            dataType: "json",
+            data: { url: pollingUrl },
+            url: "/AudioIdentification/PollIdentifySpeaker",
+            success: updateResult
+        });
+    }
+
+    function updateResult(data) {
+
+        var prettifiedResponse = syntaxHighlight(data);
+
+        prettifiedResponse = prettifiedResponse.replace(/{/g, "{<br />").replace(/}/g, "<br />}").replace(/,/g, ",<br />");
+
+        $("#status").html(prettifiedResponse);
+
+        if (data.status == "") {
+            setTimeout(pollIdentificationProcess, checkEnrollmentInterval);
+        }
+    }
+
+    function syntaxHighlight(json) {
+        if (typeof json != 'string') {
+            json = JSON.stringify(json, undefined, 2);
+        }
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
+});
