581219ceac7410b9daf240fb69d6520540c48f6e
Paul Breen <git@pbreen.co.uk>
2018-06-05 22:40:08 +0100

    csp

--- Cognitive-Azure/Startup.cs
+++ Cognitive-Azure/Startup.cs
@@ -47,6 +47,7 @@ namespace Cognitive_Azure
                    .AddCustomHeader("Content-Security-Policy", "default-src 'none\';" +
                                                                "script-src \'self\' https://cognitive.pbreen.co.uk https://maxcdn.bootstrapcdn.com https://code.jquery.com/;" +
                                                                "style-src \'self\' \'unsafe-inline\' https://maxcdn.bootstrapcdn.com;" +
+                                                               "worker-src \'self';" +
                                                                "font-src \'self\' https://maxcdn.bootstrapcdn.com;" +
                                                                "img-src \'self\' https://cognitiveservice.blob.core.windows.net;" +
                                                                "connect-src \'self\';" +
